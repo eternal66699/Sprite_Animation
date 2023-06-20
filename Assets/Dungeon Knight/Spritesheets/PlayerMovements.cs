5fc0ac5631dd54e7d347f36c4af04a5c6fd4074f
eternal66699 <julian.lopez@ciit.edu.ph>
2023-06-20 14:45:53 +0800

    Updated

---
+++ Assets/Dungeon Knight/Spritesheets/PlayerMovements.cs
@@ -0,0 +1,77 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerMovements : MonoBehaviour
+{
+    public float moveSpeed;
+    public Rigidbody2D rigidbody;
+    public Vector2 movementInput;
+    public Animator anim;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigidbody = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.W))
+        {
+            anim.SetTrigger("Backward Animation");
+        }
+
+        if (Input.GetKeyDown(KeyCode.S))
+        {
+            anim.SetTrigger("Forward Animation");
+        }
+
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            anim.SetTrigger("Left Animation");
+        }
+
+        if (Input.GetKeyDown(KeyCode.D))
+        {
+            anim.SetTrigger("Right Animation");
+        }
+
+        if (Input.GetKeyUp(KeyCode.W))
+        {
+            anim.SetTrigger("Backward Animation Pause");
+        }
+
+        if (Input.GetKeyUp(KeyCode.S))
+        {
+            anim.SetTrigger("Forward Animation Pause");
+        }
+
+        if (Input.GetKeyUp(KeyCode.A))
+        {
+            anim.SetTrigger("Left Animation Pause");
+        }
+
+        if (Input.GetKeyUp(KeyCode.D))
+        {
+            anim.SetTrigger("Right Animation Pause");
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        rigidbody.velocity = movementInput * moveSpeed;
+    }
+
+    private void LastUpdate()
+    {
+
+    }
+
+    private void OnMove (InputValue inputValue)
+    {
+        movementInput = inputValue.Get<Vector2>();
+    }
+}
