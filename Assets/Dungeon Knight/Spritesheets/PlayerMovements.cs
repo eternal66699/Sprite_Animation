30a4c2c545ce7a62408e5226576835ccc6344e73
eternal66699 <julian.lopez@ciit.edu.ph>
2023-06-27 15:05:23 +0800

    Traps and Animation acquired

--- Assets/Dungeon Knight/Spritesheets/PlayerMovements.cs
+++ Assets/Dungeon Knight/Spritesheets/PlayerMovements.cs
@@ -10,6 +10,7 @@ public class PlayerMovements : MonoBehaviour
     public Vector2 movementInput;
     public Animator anim;
     // Start is called before the first frame update
+    public int CoinCount;
     void Start()
     {
         rigidbody = GetComponent<Rigidbody2D>();
@@ -19,45 +20,48 @@ public class PlayerMovements : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.W))
-        {
-            anim.SetTrigger("Backward Animation");
-        }
+        //if (Input.GetKeyDown(KeyCode.W))
+        //{
+        //    anim.SetTrigger("Backward Animation");
+        //}
 
-        if (Input.GetKeyDown(KeyCode.S))
-        {
-            anim.SetTrigger("Forward Animation");
-        }
+        //if (Input.GetKeyDown(KeyCode.S))
+        //{
+        //    anim.SetTrigger("Forward Animation");
+        //}
 
-        if (Input.GetKeyDown(KeyCode.A))
-        {
-            anim.SetTrigger("Left Animation");
-        }
+        //if (Input.GetKeyDown(KeyCode.A))
+        //{
+        //    anim.SetTrigger("Left Animation");
+        //}
 
-        if (Input.GetKeyDown(KeyCode.D))
-        {
-            anim.SetTrigger("Right Animation");
-        }
+        //if (Input.GetKeyDown(KeyCode.D))
+        //{
+        //    anim.SetTrigger("Right Animation");
+        //}
 
-        if (Input.GetKeyUp(KeyCode.W))
-        {
-            anim.SetTrigger("Backward Animation Pause");
-        }
+        //if (Input.GetKeyUp(KeyCode.W))
+        //{
+        //    anim.SetTrigger("Backward Animation Pause");
+        //}
 
-        if (Input.GetKeyUp(KeyCode.S))
-        {
-            anim.SetTrigger("Forward Animation Pause");
-        }
+        //if (Input.GetKeyUp(KeyCode.S))
+        //{
+        //    anim.SetTrigger("Forward Animation Pause");
+        //}
 
-        if (Input.GetKeyUp(KeyCode.A))
-        {
-            anim.SetTrigger("Left Animation Pause");
-        }
+        //if (Input.GetKeyUp(KeyCode.A))
+        //{
+        //    anim.SetTrigger("Left Animation Pause");
+        //}
 
-        if (Input.GetKeyUp(KeyCode.D))
-        {
-            anim.SetTrigger("Right Animation Pause");
-        }
+        //if (Input.GetKeyUp(KeyCode.D))
+        //{
+        //    anim.SetTrigger("Right Animation Pause");
+        //}
+        anim.SetFloat("Horizontal", movementInput.x);
+        anim.SetFloat("Vertical", movementInput.y);
+        anim.SetFloat("Speed", movementInput.sqrMagnitude);
     }
 
     private void FixedUpdate()
@@ -73,5 +77,14 @@ public class PlayerMovements : MonoBehaviour
     private void OnMove (InputValue inputValue)
     {
         movementInput = inputValue.Get<Vector2>();
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Coins"))
+        {
+            Destroy(collision.gameObject);
+            CoinCount++;
+        }
     }
 }
